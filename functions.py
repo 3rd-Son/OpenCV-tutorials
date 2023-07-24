dfd5e68411947317153987f30e2bb96964be21fd
Victory Nnaji <nnajivictorious@gmail.com>
2023-07-24 22:29:23 +0100

    edge detection and blur

--- functions.py
+++ functions.py
@@ -5,4 +5,14 @@ import cv2
 img = cv2.imread("free-photo-of-people-walking-dogs-on-sidewalk-at-night.jpeg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("GRAY IMAGE", gray)
+
+
+# bluring or image
+blur_img = cv2.GaussianBlur(img, (7, 7), 0)
+cv2.imshow("BLUR IMAGE", blur_img)
+
+
+# Edge Detection
+imgCanny = cv2.Canny(img, 100, 100)
+cv2.imshow("CANNY IMAGE", imgCanny)
 cv2.waitKey(0)
