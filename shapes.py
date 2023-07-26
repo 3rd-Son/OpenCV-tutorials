1dc2507673ae041f1f25b8f44e0f42dd3445109a
Victory Nnaji <nnajivictorious@gmail.com>
2023-07-26 19:56:38 +0100

    added line and text to image

--- shapes.py
+++ shapes.py
@@ -4,9 +4,17 @@ import cv2
 img = np.zeros((500, 500, 3))
 print(img.shape)
 
-img[:] = 255, 0, 0
-
-cv2.imshow("image", img)
-cv2.waitKey(0)
 
 # adding color to image
+img[:] = 255, 0, 0
+
+
+# adding line
+cv2.line(img, (0, 0), (200, 200), (0, 0, 255))
+
+# adding text
+cv2.putText(
+    img, "This is a text", (150, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (30, 255, 0)
+)
+cv2.imshow("image", img)
+cv2.waitKey(0)
